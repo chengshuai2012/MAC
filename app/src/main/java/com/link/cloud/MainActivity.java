1bb1d8c5faa0c4d09d261cfdf9a54142cae55742
chengshuai <494886288@qq.com>
2018-09-20 18:26:31 +0800

    第一次提交

---
+++ app/src/main/java/com/link/cloud/MainActivity.java
@@ -0,0 +1,62 @@
+package com.link.cloud;
+
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.link.cloud.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.member)
+    TextView member;
+    @BindView(R.id.manager)
+    TextView manager;
+    @BindView(R.id.choose_lesson)
+    TextView chooseLesson;
+    @BindView(R.id.lesson_in)
+    TextView lessonIn;
+    @BindView(R.id.choose_lesson_container)
+    LinearLayout chooseLessonContainer;
+    @BindView(R.id.fg_container)
+    FrameLayout fgContainer;
+
+    @Override
+    protected int setLayoutID() {
+        return R.layout.activity_main;
+    }
+
+    @OnClick({R.id.member, R.id.manager, R.id.lesson_in, R.id.choose_lesson})
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.member:
+                member.setBackground(getResources().getDrawable(R.drawable.border_red));
+                manager.setBackground(null);
+                member.setTextColor(getResources().getColor(R.color.almost_white));
+                manager.setTextColor(getResources().getColor(R.color.text_gray));
+                break;
+            case R.id.manager:
+                manager.setBackground(getResources().getDrawable(R.drawable.border_red));
+                member.setBackground(null);
+                member.setTextColor(getResources().getColor(R.color.text_gray));
+                manager.setTextColor(getResources().getColor(R.color.almost_white));
+                break;
+            case R.id.lesson_in:
+                lessonIn.setBackground(getResources().getDrawable(R.drawable.border_red_half_right));
+                chooseLesson.setBackground(null);
+                lessonIn.setTextColor(getResources().getColor(R.color.almost_white));
+                chooseLesson.setTextColor(getResources().getColor(R.color.red));
+                break;
+            case R.id.choose_lesson:
+                chooseLesson.setBackground(getResources().getDrawable(R.drawable.border_red_half_left));
+                lessonIn.setBackground(null);
+                lessonIn.setTextColor(getResources().getColor(R.color.red));
+                chooseLesson.setTextColor(getResources().getColor(R.color.almost_white));
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/link/cloud/base/BaseActivity.java
@@ -0,0 +1,36 @@
+package com.link.cloud.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by OFX002 on 2018/9/20.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity{
+
+    private Unbinder bind;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(setLayoutID());
+        bind = ButterKnife.bind(this);
+    }
+
+    protected abstract int setLayoutID();
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        bind.unbind();
+    }
+    protected void showToast(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    }
+}
