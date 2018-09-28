11f02b6aadc12f4ef6386c92bbd5e2bfe365e8e4
luqian <zar.l@qq.com>
2018-09-28 09:50:38 +0800

    增加了单场券的购买

---
+++ app/src/main/java/com/link/cloud/activity/SinglePurchaseActivity.java
@@ -0,0 +1,50 @@
+package com.link.cloud.activity;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.link.cloud.R;
+import com.link.cloud.base.AppBarActivity;
+
+/**
+ * @author qianlu
+ * @date 2018/9/27.
+ * GitHub：qiandailu
+ * email：zar.l@qq.com
+ * description：
+ */
+public class SinglePurchaseActivity extends AppBarActivity {
+
+    private RadioButton singBuy;
+    private RadioButton activiteCard;
+    private RadioButton rechargeCard;
+    private RadioGroup radioGroup;
+    private FrameLayout contentframe;
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_singlepurchase;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initialize();
+    }
+
+    private void initialize() {
+
+        singBuy = (RadioButton) findViewById(R.id.singBuy);
+        activiteCard = (RadioButton) findViewById(R.id.activiteCard);
+        rechargeCard = (RadioButton) findViewById(R.id.rechargeCard);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        contentframe = (FrameLayout) findViewById(R.id.content_frame);
+    }
+}
