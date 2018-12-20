e2d42d3afeb214aadf35b68fe93921dc8016fe48
chengshuai_cs <494886288@qq.com>
2018-12-20 16:28:01 +0800

    色系修改

--- app/src/main/java/com/link/cloud/utils/Venueutils.java
+++ app/src/main/java/com/link/cloud/utils/Venueutils.java
@@ -1,4 +1,5 @@
 package com.link.cloud.utils;
+
 import android.app.Service;
 import android.content.ComponentName;
 import android.content.Context;
@@ -47,18 +48,21 @@ public class Venueutils {
     private int modOkProgress = 0;
     private final static float IDENTIFY_SCORE_THRESHOLD = 0.63f;
     private final static float MODEL_SCORE_THRESHOLD = 0.4f;
-    public interface VenueCallBack{
-        void modelMsg(int state,String msg);
+
+    public interface VenueCallBack {
+        void modelMsg(int state, String msg);
     }
-    public  void initVenue(Context context, VenueCallBack callBack,  Boolean bOpen){
-        this.bOpen=bOpen;
-        this.context=context;
-        if(mdDeviceBinder==null){
+
+    public void initVenue(Context context, VenueCallBack callBack, Boolean bOpen) {
+        this.bOpen = bOpen;
+        this.context = context;
+        if (mdDeviceBinder == null) {
             Intent intent = new Intent(context, MdUsbService.class);
             context.bindService(intent, mdSrvConn, Service.BIND_AUTO_CREATE);
         }
-        this.callBack=callBack;
+        this.callBack = callBack;
     }
+
     public int getState() {
         if (!bOpen) {
             modOkProgress = 0;
@@ -100,12 +104,12 @@ public class Venueutils {
         String oneResult = ("quality return=" + quaRtn) + ",result=" + quaScore[0] + ",score=" + quaScore[1] + ",fLeakRatio=" + quaScore[2] + ",fPress=" + quaScore[3];
         int quality = (int) quaScore[0];
         if (quality != 0) {
-            //todo
-return;
+            TTSUtils.getInstance().speak(context.getString(R.string.image_fail));
+            return;
         }
         byte[] feature = MdUsbService.extractImgModel(img, null, null);
         if (feature == null) {
-//todo
+
         } else {
             modOkProgress++;
             if (modOkProgress == 1) {//first model
@@ -113,7 +117,7 @@ return;
                 tipTimes[1] = 0;
                 modelImgMng.setImg1(img);
                 modelImgMng.setFeature1(feature);
-                callBack.modelMsg(1,context.getString(R.string.again_finger));
+                callBack.modelMsg(1, context.getString(R.string.again_finger));
             } else if (modOkProgress == 2) {//second model
                 ret = MdUsbService.fvSearchFeature(modelImgMng.getFeature1(), 1, img, pos, score);
                 if (ret && score[0] > MODEL_SCORE_THRESHOLD) {
@@ -123,26 +127,26 @@ return;
                         tipTimes[1] = 0;
                         modelImgMng.setImg2(img);
                         modelImgMng.setFeature2(feature);
-                        callBack.modelMsg(1,context.getString(R.string.again_finger));
+                        callBack.modelMsg(1, context.getString(R.string.again_finger));
                     } else {//第二次建模从图片中取特征值无效
                         modOkProgress = 1;
                         if (++tipTimes[0] <= 3) {
-                            callBack.modelMsg(2,context.getString(R.string.same_finger));
+                            callBack.modelMsg(2, context.getString(R.string.same_finger));
 
                         } else {//连续超过3次放了不同手指则忽略此次建模重来
                             modOkProgress = 0;
                             modelImgMng.reset();
-                            callBack.modelMsg(2,context.getString(R.string.same_finger));
+                            callBack.modelMsg(2, context.getString(R.string.same_finger));
                         }
                     }
                 } else {
                     modOkProgress = 1;
                     if (++tipTimes[0] <= 3) {
-                        callBack.modelMsg(2,context.getString(R.string.same_finger));
+                        callBack.modelMsg(2, context.getString(R.string.same_finger));
                     } else {//连续超过3次放了不同手指则忽略此次建模重来
                         modOkProgress = 0;
                         modelImgMng.reset();
-                        callBack.modelMsg(2,context.getString(R.string.same_finger));
+                        callBack.modelMsg(2, context.getString(R.string.same_finger));
                     }
                 }
             } else if (modOkProgress == 3) {//third model
@@ -153,7 +157,7 @@ return;
                         tipTimes[0] = 0;
                         tipTimes[1] = 0;
                         modelImgMng.setImg3(img);
-                        callBack.modelMsg(3,HexUtil.bytesToHexString(feature));
+                        callBack.modelMsg(3, HexUtil.bytesToHexString(feature));
                         modelImgMng.setFeature3(feature);
                         modelImgMng.reset();
                         //mdDeviceBinder.closeDevice(0);
@@ -161,17 +165,17 @@ return;
                     } else {//第三次建模从图片中取特征值无效
                         modOkProgress = 2;
                         if (++tipTimes[1] <= 3) {
-                            callBack.modelMsg(2,context.getString(R.string.same_finger));
+                            callBack.modelMsg(2, context.getString(R.string.same_finger));
                         }
                     }
                 } else {
                     modOkProgress = 2;
                     if (++tipTimes[1] <= 3) {
-                        callBack.modelMsg(2,context.getString(R.string.same_finger));
+                        callBack.modelMsg(2, context.getString(R.string.same_finger));
                     } else {//连续超过3次放了不同手指则忽略此次建模重来
                         modOkProgress = 0;
                         modelImgMng.reset();
-                        callBack.modelMsg(2,context.getString(R.string.same_finger));
+                        callBack.modelMsg(2, context.getString(R.string.same_finger));
                     }
                 }
             } else {
@@ -181,23 +185,25 @@ return;
         }
 
     }
+
     List<AllUser> subListPeople = new ArrayList<>();
+
     public String identifyNewImg(final List<AllUser> peoples) {
-        final int nThreads=peoples.size()/1000+1;
+        final int nThreads = peoples.size() / 1000 + 1;
         ExecutorService executorService = Executors.newFixedThreadPool(3);
         List<Future<String>> futures = new ArrayList();
         for (int i = 0; i < nThreads; i++) {
-                if(i==nThreads-1){
-                    subListPeople= peoples.subList(1000 * i, peoples.size());
-                }else {
-                    subListPeople= peoples.subList(1000 * i, 1000 * (i + 1));
-                }
+            if (i == nThreads - 1) {
+                subListPeople = peoples.subList(1000 * i, peoples.size());
+            } else {
+                subListPeople = peoples.subList(1000 * i, 1000 * (i + 1));
+            }
             Callable<String> task = new Callable<String>() {
                 @Override
                 public String call() throws Exception {
                     StringBuffer sb = new StringBuffer();
                     String[] uids = new String[1000];
-                    int position =0;
+                    int position = 0;
                     for (AllUser userBean : subListPeople) {
                         sb.append(userBean.getFingerprint());
                         uids[position] = userBean.getUuid();
@@ -222,7 +228,7 @@ return;
         }
         for (Future<String> future : futures) {
             try {
-                if(!TextUtils.isEmpty(future.get())){
+                if (!TextUtils.isEmpty(future.get())) {
                     return future.get();
                 }
             } catch (InterruptedException e) {
@@ -234,16 +240,18 @@ return;
         executorService.shutdown();
         return null;
     }
+
     List<GroupLessonUser> subListUser = new ArrayList<>();
+
     public String identifyNewImgUser(final ArrayList<GroupLessonUser> peoples) {
-        final int nThreads=peoples.size()/1000+1;
+        final int nThreads = peoples.size() / 1000 + 1;
         ExecutorService executorService = Executors.newFixedThreadPool(3);
         List<Future<String>> futures = new ArrayList();
         for (int i = 0; i < nThreads; i++) {
-            if(i==nThreads-1){
-                subListUser= peoples.subList(1000 * i, peoples.size());
-            }else {
-                subListUser= peoples.subList(1000 * i, 1000 * (i + 1));
+            if (i == nThreads - 1) {
+                subListUser = peoples.subList(1000 * i, peoples.size());
+            } else {
+                subListUser = peoples.subList(1000 * i, 1000 * (i + 1));
             }
 
             Callable<String> task = new Callable<String>() {
@@ -251,7 +259,7 @@ return;
                 public String call() throws Exception {
                     StringBuffer sb = new StringBuffer();
                     String[] uids = new String[1000];
-                    int position =0;
+                    int position = 0;
                     for (GroupLessonUser userBean : subListUser) {
                         sb.append(userBean.getFingerprint());
                         uids[position] = userBean.getUuid();
@@ -276,7 +284,7 @@ return;
         }
         for (Future<String> future : futures) {
             try {
-                if(!TextUtils.isEmpty(future.get())){
+                if (!TextUtils.isEmpty(future.get())) {
                     return future.get();
                 }
             } catch (InterruptedException e) {
@@ -288,6 +296,7 @@ return;
         executorService.shutdown();
         return null;
     }
+
     private List<MdDevice> mdDevicesList = new ArrayList<MdDevice>();
     public static MdDevice mdDevice;
     private final int MSG_REFRESH_LIST = 0;
@@ -315,6 +324,7 @@ return;
         }
 
     });
+
     private List<MdDevice> getDevList() {
         List<MdDevice> mdDevList = new ArrayList<MdDevice>();
         if (mdDeviceBinder != null) {
@@ -326,11 +336,12 @@ return;
                 mdDevList.add(mdDevice);
             }
         } else {
-            Logger.e( "microFingerVein not initialized by MdUsbService yet,wait a moment...");
+            Logger.e("microFingerVein not initialized by MdUsbService yet,wait a moment...");
         }
         return mdDevList;
 
     }
+
     private ServiceConnection mdSrvConn = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
@@ -340,9 +351,10 @@ return;
                 listManageH.sendEmptyMessage(MSG_REFRESH_LIST);
                 Logger.e("bind MdUsbService success.");
             } else {
-                Logger.e( "bind MdUsbService failed.");
+                Logger.e("bind MdUsbService failed.");
             }
         }
+
         @Override
         public void onServiceDisconnected(ComponentName name) {
             Logger.e("disconnect MdUsbService.");
@@ -355,13 +367,15 @@ return;
             String newUsbInfo = "USB厂商：" + usbManufacturerName + "  \nUSB节点：" + usbDeviceName;
             Logger.e(newUsbInfo);
         }
+
         @Override
         public void onUsbDisconnect() {
             Logger.e("USB连接已断开");
         }
 
     };
-    public void unBindService(){
+
+    public void unBindService() {
         context.unbindService(mdSrvConn);
     }
 
